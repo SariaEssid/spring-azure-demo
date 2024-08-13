b9359355fad238ea1880d0ae633e675440966dbf
SariaEssid <saria.essid@esprit.tn>
2024-08-13 03:33:38 +0100

    forth commit

--- src/main/java/com/javatechi/azure/demo/TestController.java
+++ src/main/java/com/javatechi/azure/demo/TestController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
  
-    @GetMapping("/message") 
+    @GetMapping("/") 
     public String message()
     {
         return "Hello AZURE - ESSID from Controller!.";
